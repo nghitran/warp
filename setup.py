9faac99aed0ed92f2463ff56c4c3b90ddd605cd8
Jake Morrison <jake@cogini.com>
2020-04-10 10:55:56 +0800

    Require twisted 12.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(name="warp",
       version="0.2.3",
-      zip_safe = False,
+      zip_safe=False,
       include_package_data=True,
 
       description="Easy-to-use layer over twisted.web",
@@ -13,17 +13,17 @@ setup(name="warp",
 
       download_url="https://github.com/brendonh/warp/tarball/v0.2.3",
 
-      packages = find_packages('.') + ["twisted.plugins"],
+      packages=find_packages('.') + ["twisted.plugins"],
 
-      install_requires = [
-          "twisted >= 8.2",
+      install_requires=[
+          "twisted >= 12.3",
           "storm >= 0.12",
           "Mako >= 0.2.5",
           "pytz",
           "simplejson",
       ],
 
-      package_data = {
+      package_data={
           "twisted": ['plugins/warp_plugin.py'],
       }
 )
