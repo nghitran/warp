b7e01adb4895b462a38a93ad5f874ab970efd47f
Jake Morrison <jake@cogini.com>
2020-04-17 13:43:39 +0800

    Formatting

--- warp/crud/model.py
+++ warp/crud/model.py
@@ -9,7 +9,6 @@ from warp.crud import colproxy, columns
 from warp import helpers
 
 class CrudModel(object):
-
     editRenderers = {
         Int: colproxy.IntProxy,
         Unicode: colproxy.StringProxy,
@@ -53,26 +52,22 @@ class CrudModel(object):
     def __init__(self, obj):
         self.obj = obj
 
-
     def name(self, request):
         return self.obj.id
-
 
     def parentCrumb(self, request):
         parent = self.parent(request)
         if parent is not None:
             return helpers.getCrudObj(parent)
 
-
     def parent(self, request):
         return None
 
     def linkAsParent(self, request):
-        return helpers.link(self.name(request), 
-                            helpers.getCrudNode(self), 
-                            "view", 
+        return helpers.link(self.name(request),
+                            helpers.getCrudNode(self),
+                            "view",
                             [self.obj.id])
-
 
     def saveRedirect(self, request):
         return helpers.url(request.node, 'view', request.resource.args)
@@ -83,13 +78,11 @@ class CrudModel(object):
             return getattr(self, funcName)(request)
         return self.defaultProxy(colName)
 
-
     def defaultProxy(self, colName):
         val = getattr(self.obj, colName)
         # Avoid triggering the property __get__
         valType = self.obj.__class__.__dict__[colName].__class__
         return self.editRenderers[valType](self.obj, colName)
-
 
     def renderListView(self, colName, request):
         funcName = "render_list_%s" % colName
@@ -97,13 +90,11 @@ class CrudModel(object):
             return getattr(self, funcName)(request)
         return self.getProxy(colName, request).render_view(request)
 
-
     def renderView(self, colName, request):
         funcName = "render_%s" % colName
         if hasattr(self, funcName):
             return getattr(self, funcName)(request)
         return self.getProxy(colName, request).render_view(request)
-
 
     def renderEdit(self, colName, request):
         funcName = "render_edit_%s" % colName
@@ -111,14 +102,12 @@ class CrudModel(object):
             return getattr(self, funcName)(request)
         return self.getProxy(colName, request).render_edit(request)
 
-        
     def save(self, colName, val, request):
         funcName = "save_%s" % colName
         if hasattr(self, funcName):
             return getattr(self, funcName)(val, request)
         return self.getProxy(colName, request).save(val, request)
 
-    
     @classmethod
     def listConditions(cls, model, request):
         conditions = []
